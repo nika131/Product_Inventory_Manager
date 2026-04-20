f3a03fd1bd3a00dcc79cca443dc48614e2f4210d
nika131 <nmegrelishvili313@gmail.com>
2026-04-20 23:58:23 +0400

    feat: implement DatabaseHelper and configure SQL connection string

---
+++ Product_Inventory_Manager/Data/DatabaseHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
+
+namespace Product_Inventory_Manager.Data
+{
+    internal class DatabaseHelper
+    {
+        public static string connString = ConfigurationManager.ConnectionStrings["InventoryDb"].ConnectionString;
+
+        public static DataTable ExecuteStoredProcedure(string procedureName, Dictionary<string, object> parametrs = null)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand(procedureName, conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    if (parametrs != null)
+                    {
+                        foreach (var param in parametrs)
+                        {
+                            cmd.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+                        }
+                    }
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+
+        public static int ExecuteNonQuery(string procedureName, Dictionary<string, object> parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand(procedureName, conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    foreach (var param in parameters)
+                    {
+                        cmd.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+                    }
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
